e62bdc275e77bf312958211c2c8ea56d5a7f7360
federicapaglia96 <federicapaglia96@gmail.com>
2025-07-01 18:33:24 +0200

    moviesreact

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -16,9 +16,13 @@ function Movies() {
         <main>
             <section className="container py-5">
                 <h1 className="text-center mb-4">Lista Film</h1>
-                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
                     {movies.map((curMovie) => {
-                        return <MoviesCard key={curMovie.id} movie={curMovie}/>
+                        return (
+                            <div className="col d-flex">
+                                <MoviesCard key={curMovie.id} movie={curMovie} />
+                            </div>
+                        );
                     })}
                 </div>
             </section>
